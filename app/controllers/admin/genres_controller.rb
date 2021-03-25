323d0b663a1496b0c6eaba9cc48d4c335bd1e3f7
Sa110-5325 <sa110.35325@gmail.com>
2021-03-25 13:31:28 +0000

    [add]ジャンル保存

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,10 +1,29 @@
 class Admin::GenresController < ApplicationController
   def index
-  end
-
-  def show
+    @genre = Genre.new
+    @genres = Genre.all
   end
 
   def edit
+    @genre = Genre.find(params[:id])
+  end
+
+  def create
+    @genres = Genre.all
+    @genre = Genre.new(genre_params)
+    if @genre.save
+      redirect_to request.referer
+    else
+      render "index"
+    end
+  end
+
+  def update
+  end
+
+  private
+
+  def genre_params
+    params.require(:genre).permit(:name)
   end
 end
