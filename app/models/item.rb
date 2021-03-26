be5cd9f40463bfcb63b43e1c2a500cfcf2e4acad
Sa110-5325 <sa110.35325@gmail.com>
2021-03-26 07:23:40 +0000

    [add]ジャンル編集

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,5 @@
 class Item < ApplicationRecord
+  has_many :carts
+  has_many :orderd_items
+  has_many :genres
 end
--- app/models/ordered_item.rb
+++ app/models/ordered_item.rb
@@ -1,2 +1,5 @@
 class OrderedItem < ApplicationRecord
+  belongs_to :order
+  belongs_to :item
+  enum status: { 制作不可: 0, 制作待ち: 1, 製作中: 2, 制作完了: 3 }
 end
