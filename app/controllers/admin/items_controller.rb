17fce8448157d9605c42f4cd0fc53f43e20596f7
Sa110-5325 <sa110.35325@gmail.com>
2021-03-26 15:22:27 +0000

    [add]admin商品一覧

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,35 +1,38 @@
 class Admin::ItemsController < ApplicationController
-  
+  before_action :authenticate_admin!
+
   def index
     @items = Item.all
   end
-  
+
   def show
+    @item = Item.find(params[:id])
   end
-  
+
   def edit
+    @item = Item.find(params[:id])
   end
-  
+
   def new
     @item = Item.new
     @genres = Genre.all
   end
-  
+
   def create
     @genres = Genre.all
     @item = Item.new(item_params)
     if @item.save
-      redirect_to admin_items_path(@item)
+      redirect_to admin_item_path(@item)
     else
       render "new"
     end
   end
-  
+
   def update
   end
-  
+
   private
-  
+
   def item_params
     params.require(:item).permit(:image, :name, :info, :genre_id, :price, :status)
   end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
   has_many :carts
   has_many :orderd_items
-  has_many :genres
+  belongs_to :genre
   attachment :image
 end
