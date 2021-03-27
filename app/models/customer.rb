ab6d6798f96ed18077858b6d38f377244cbbd0cc
Sa110-5325 <sa110.35325@gmail.com>
2021-03-27 14:11:09 +0000

    [add]customer商品一覧

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -6,5 +6,5 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+  attachment :image
 end
