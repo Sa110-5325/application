aaea42b77dea04b1e5d14a21bb2ff3002d5f708a
Sa110-5325 <sa110.35325@gmail.com>
2021-03-27 09:12:47 +0000

    [add]customer会員情報編集

--- app/controllers/customers/customers_controller.rb
+++ app/controllers/customers/customers_controller.rb
@@ -6,4 +6,19 @@ class Customers::CustomersController < ApplicationController
   def edit
     @customer = current_customer
   end
+
+  def update
+    @customer = current_customer
+    if @customer.update(customer_params)
+      redirect_to customer_path(@customer)
+    else
+      render "edit"
+    end
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:family_name, :first_name, :family_name_kana, :first_name_kana, :postal_code, :address, :phone_number, :email)
+  end
 end
